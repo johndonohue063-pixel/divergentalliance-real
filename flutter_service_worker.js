8067f5cabadca3fb82f4367296bde53ee4b0bf6b
John Donohue <john.donohue@divergentalliance.com>
2025-12-04 00:06:43 -0600

    Update web build after runApp fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,14 +41,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "d0b5c70b300818d28242939f4cb48373
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "583b6448fe5bd37ddf23e42f48eca4f6",
+"flutter_bootstrap.js": "5f219e250f5cd323f95e163e0e6c2367",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "fa3c76b4207fb8bb759b0278a9dae634",
 "/": "fa3c76b4207fb8bb759b0278a9dae634",
-"main.dart.js": "18bc13d33bca3121ce95464097d0dec5",
+"main.dart.js": "c15686a129932657be9ea3b70342514f",
 "manifest.json": "975bef76b7297717c8f4a7690da5d84a",
 "version.json": "b3ff247f436397ac1acd3734820dcf05"};
 // The application shell files that are downloaded before a service worker can
